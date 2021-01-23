62e080925d16e8f93752e9ed21aebcdd8449427f
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-23 21:25:57 +0530

    Added: Full time Employee wage

--- EmpWage.java
+++ EmpWage.java
@@ -3,12 +3,18 @@ public class EmpWage {
 	public static void main(String[] args) {
 		// Constant
 		int FULL_TIME = 1;
+		int EMP_RATE_PER_HR = 20;
+		// Variale
+		int emp_hr = 8;
+		int empwage = 0;
 		double empcheck = Math.floor(Math.random() * 10) % 2;
 		// check Present or absent
 		if (empcheck == FULL_TIME)
-			System.out.println("Employee is present");
+			emp_hr = 8;
 		else
-			System.out.println("Emploee is absent");
+			emp_hr = 0;
+		empwage = emp_hr * EMP_RATE_PER_HR;
+		System.out.println("Employee Wage: "+empwage);
 	}
 
 }
