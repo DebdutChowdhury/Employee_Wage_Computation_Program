c76f5673fabd7b2f738106901fc2ae472f2b3170
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-23 21:47:45 +0530

    Added: Computation part using switch case

--- EmpWage.java
+++ EmpWage.java
@@ -1,21 +1,22 @@
 public class EmpWage {
-
+	static final int PART_TIME = 1;
+	static final int FULL_TIME = 2;
+	static int EMP_RATE_PER_HR = 20;
+	
 	public static void main(String[] args) {
-		// Constant
-		int FULL_TIME = 1;
-		int PART_TIME = 2;
-		int EMP_RATE_PER_HR = 20;
 		// Variale
 		int emp_hr = 8;
 		int empwage = 0;
 		double empcheck = Math.floor(Math.random() * 10) % 3;
 		// check Present or absent
-		if (empcheck == FULL_TIME)
-			emp_hr = 8;
-		else if (empcheck == PART_TIME)
-			emp_hr = 4;
-		else
-			emp_hr = 0;
+		switch((int)empcheck)
+		{
+		case FULL_TIME: emp_hr = 8;
+			break;
+		case PART_TIME: emp_hr = 4;
+			break;
+		default: emp_hr = 0;
+		}
 		empwage = emp_hr * EMP_RATE_PER_HR;
 		System.out.println("Employee Wage: "+empwage);
 	}
