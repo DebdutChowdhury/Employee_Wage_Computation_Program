0591b4dc7afa58ec34e2fbf310c9490fa447d97c
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-23 22:11:19 +0530

    Added: Employee monthly wage

--- EmpWage.java
+++ EmpWage.java
@@ -2,11 +2,13 @@ public class EmpWage {
 	static final int PART_TIME = 1;
 	static final int FULL_TIME = 2;
 	static int EMP_RATE_PER_HR = 20;
+	static int WORK_MONTH = 20;
 	
 	public static void main(String[] args) {
 		// Variale
 		int emp_hr = 8;
 		int empwage = 0;
+		int total_month = 0; 
 		double empcheck = Math.floor(Math.random() * 10) % 3;
 		// check Present or absent
 		switch((int)empcheck)
@@ -18,7 +20,8 @@ public class EmpWage {
 		default: emp_hr = 0;
 		}
 		empwage = emp_hr * EMP_RATE_PER_HR;
-		System.out.println("Employee Wage: "+empwage);
+		total_month = empwage * WORK_MONTH;
+		System.out.println("Employee Wage per day: "+empwage);
+		System.out.println("Employee wage per month: "+total_month);
 	}
-
 }
