1b356087e89a5910e2b970bd01ef60caeb756d91
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-23 20:30:16 +0530

    Added: welcome message here

---
+++ EmpWage.java
@@ -0,0 +1,10 @@
+public class EmpWage {
+
+	public static void main(String[] args) {
+		// Welcome message here
+		System.out.println("**********************************");
+		System.out.println("welcome in the employe wage prolem");
+		System.out.println("**********************************");
+	}
+
+}
