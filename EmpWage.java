c63fc8ef36d90877f44e239a1488597c89643f38
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-24 01:12:26 +0530

    Added: Calculate Wages till a condition

--- EmpWage.java
+++ EmpWage.java
@@ -2,26 +2,36 @@ public class EmpWage {
 	static final int PART_TIME = 1;
 	static final int FULL_TIME = 2;
 	static int EMP_RATE_PER_HR = 20;
-	static int WORK_MONTH = 20;
+	static int MAX_WORK_MONTH = 20;
+	static int MAX_WORK_HR = 100;
 	
 	public static void main(String[] args) {
 		// Variale
 		int emp_hr = 8;
-		int empwage = 0;
-		int total_month = 0; 
-		double empcheck = Math.floor(Math.random() * 10) % 3;
+		int total_emp_hr = 0; 
+		int total_emp_days = 0;
+		int total_wage = 0;
 		// check Present or absent
-		switch((int)empcheck)
+		while (total_emp_days < MAX_WORK_MONTH && total_emp_hr <= MAX_WORK_HR)
 		{
-		case FULL_TIME: emp_hr = 8;
-			break;
-		case PART_TIME: emp_hr = 4;
-			break;
-		default: emp_hr = 0;
+			
+			double empcheck = Math.floor(Math.random()*10)%3;
+			total_emp_days++;
+			switch ((int)empcheck)
+			{
+			case FULL_TIME: emp_hr = 8;
+				break;
+			case PART_TIME: emp_hr = 4;
+				break;
+			default: emp_hr = 0;
+			}
+			total_emp_hr += emp_hr;
+			System.out.println("Total Employee Hours: "+total_emp_hr);
+			System.out.println("Total Employee Days: "+total_emp_days);
+			
 		}
-		empwage = emp_hr * EMP_RATE_PER_HR;
-		total_month = empwage * WORK_MONTH;
-		System.out.println("Employee Wage per day: "+empwage);
-		System.out.println("Employee wage per month: "+total_month);
+		total_wage = total_emp_hr * EMP_RATE_PER_HR;
+		System.out.println("Total Employee Wage: "+total_wage);
 	}
+
 }
