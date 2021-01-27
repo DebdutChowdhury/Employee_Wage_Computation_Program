a2559ddbabee96546884d4347f9c9c604c55a607
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-27 21:47:05 +0530

    Added: class and methods in this Employee wage problem

--- EmpWage.java
+++ EmpWage.java
@@ -1,12 +1,11 @@
-public class EmpWage {
-	static final int PART_TIME = 1;
-	static final int FULL_TIME = 2;
-	static int EMP_RATE_PER_HR = 20;
-	static int MAX_WORK_MONTH = 20;
-	static int MAX_WORK_HR = 100;
+class EmpDetail{
+	final int PART_TIME = 1;
+	final int FULL_TIME = 2;
+	int EMP_RATE_PER_HR = 20;
+	int MAX_WORK_MONTH = 20;
+	int MAX_WORK_HR = 100;
 	
-	public static void main(String[] args) {
-		// Variale
+	public void calculate() {
 		int emp_hr = 8;
 		int total_emp_hr = 0; 
 		int total_emp_days = 0;
@@ -19,19 +18,38 @@ public class EmpWage {
 			total_emp_days++;
 			switch ((int)empcheck)
 			{
-			case FULL_TIME: emp_hr = 8;
+			case FULL_TIME: emp_hr = 8; // case statement for full time employe
 				break;
-			case PART_TIME: emp_hr = 4;
+			case PART_TIME: emp_hr = 4; // case statement for part time employe
 				break;
 			default: emp_hr = 0;
 			}
-			total_emp_hr += emp_hr;
+			total_emp_hr += emp_hr; 
 			System.out.println("Total Employee Hours: "+total_emp_hr);
 			System.out.println("Total Employee Days: "+total_emp_days);
 			
 		}
-		total_wage = total_emp_hr * EMP_RATE_PER_HR;
+		total_wage = total_emp_hr * EMP_RATE_PER_HR; // calculate total employe wage
 		System.out.println("Total Employee Wage: "+total_wage);
 	}
-
 }
+
+
+public class EmpWage {
+	
+	
+	public static void main(String[] args) {
+		
+		EmpDetail ed = new EmpDetail(); 
+		ed.calculate();
+	}
+	
+}
+
+
+
+
+
+
+
+
